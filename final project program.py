f40bc00d304a9cd51bf40b79d0d24dfc25ed13ff
Alex <kwunlok.tse21@ycdsbk12.ca>
2019-05-21 09:51:29 -0400

    set up the frame for the programme and testing on the rotating chracter

---
+++ final project program.py
@@ -0,0 +1,80 @@
+import arcade
+from math import*
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+
+class MyGame(arcade.Window):
+
+    def __init__(self):
+        # initialize the screen
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, 'RPG game')
+
+        # sprite lists
+        self.player_list = None
+        self.enemy_list = None
+        self.player_bullet_list = None
+        self.enemy_bullet_list = None
+        self.wall_list = None
+
+        # player sprite
+        self.player_sprite = None
+
+        # physics engine
+        self.physics_engine = None
+
+        # manage the view point
+        self.view_left = 0
+        self.view_bottom = 0
+
+        # setup the score
+        self.score = 0
+
+    def setup(self):
+
+        # set background color
+        arcade.set_background_color(arcade.color.WHITE)
+
+        # Reset the view port
+        self.view_left = 0
+        self.view_bottom = 0
+
+        # setup sprite lists
+        self.player_list = arcade.SpriteList()
+        self.enemy_list = arcade.SpriteList()
+        self.player_bullet_list = arcade.SpriteList()
+        self.enemy_bullet_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+
+        # reset the score
+        self.score = 0
+
+        # create the player
+        self.player_sprite = arcade.Sprite('image/rouge.png', 0.3)
+        self.player_sprite.center_x = 500
+        self.player_sprite.center_y = 400
+        self.player_list.append(self.player_sprite)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        if x - self.player_sprite.center_x == 0:
+            if y > self.player_sprite.center_y:
+                self.player_sprite.angle = 90
+            else:
+                self.player_sprite.angle = 270
+        else:
+            self.player_sprite.angle = degrees(atan((y - self.player_sprite.center_y)/(x - self.player_sprite.center_x)))
+            print(degrees(atan((y - self.player_sprite.center_y)/(x - self.player_sprite.center_x))))
+
+    def on_draw(self):
+        arcade.start_render()
+        self.player_list.draw()
+
+    def update(self, delta_time):
+        self.player_list.update()
+
+def main():
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+main()
